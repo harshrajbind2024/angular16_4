cbc02178be873f49bc4a06e1428cac233b377378
harshrajbind2023 <harshrajbind2023@gmail.com>
2024-12-12 09:58:57 +0530

    4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fourth';
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  Register(val:any){
+
+    // // debug 
+    // debugger;
+    // var firstname=val.controls.firstname.value;
+    // var lastname=val.controls.lastname.value;
+    // var email=val.controls.Email.value;
+    //   console.log(firstname);
+      
+  
+    console.log("regForm",val);
+    
+
+  }
 }
